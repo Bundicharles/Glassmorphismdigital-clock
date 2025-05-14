25b2a32bc010175976af509985d9e99d499921ea
Bundicharles <bundimunenecharles6206@gmail.com>
2025-05-14 18:46:57 +0300

    all

--- script.js
+++ script.js
@@ -110,4 +110,13 @@ function updateClock() {
     setInterval(updateClock, 1000);
     viewSavedHistory();
   };
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(reg => {
+        console.log('Service worker registered.', reg);
+      }).catch(err => {
+        console.error('Service worker registration failed:', err);
+      });
+    });
+  }
   
--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = 'clock-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('pwa-clock-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          '/manifest.json',
+          '/icon-192.png',
+          '/icon-512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then(response => response || fetch(e.request))
+    );
+  });
+  
